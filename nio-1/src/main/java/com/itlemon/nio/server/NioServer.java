c61209d9e39d97e43898d17d0538a8ee7ba5b365
lemon-zf <zhang91026@gmail.com>
2016-08-22 18:05:30 +0800

    初始化

--- nio-1/src/main/java/com/itlemon/nio/server/NioServer.java
+++ nio-1/src/main/java/com/itlemon/nio/server/NioServer.java
@@ -2,9 +2,12 @@ package com.itlemon.nio.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -13,16 +16,13 @@ import java.util.Set;
  */
 public class NioServer {
 
-    private String host;
-
     private int port;
 
     private ServerSocketChannel ssc;
 
     private Selector selector;
 
-    public NioServer(String host, int port) {
-        this.host = host;
+    public NioServer(int port) {
         this.port = port;
     }
 
@@ -38,46 +38,80 @@ public class NioServer {
 
         ssc.register(selector,SelectionKey.OP_ACCEPT);
 
+        
         System.out.println("==>server is start");
+        
+        for (;;) {
+
+            try {
+
+                selector.select(1000l);
+
+                Set<SelectionKey> keys = selector.selectedKeys();
+
+                Iterator<SelectionKey> ketIt = keys.iterator();
+
+                while (ketIt.hasNext()){
+                	
+                    SelectionKey key = ketIt.next();
+                    
+                    ketIt.remove();
+                    
+                    if(key.isAcceptable()){
+                    	accep(key);
+                    }
+                    
+                    if(key.isWritable()){
+                    	write(key);
+                    }
+                    
+                    if(key.isReadable()){
+                    	read(key);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
-    class Dispatcher implements Runnable {
-
-        public void run() {
-
-            for (;;) {
-
-                try {
-
-                    selector.select(1000l);
-
-                    selector.wakeup();
-
-                    Set<SelectionKey> keys = selector.selectedKeys();
-
-                    Iterator<SelectionKey> ketIt = keys.iterator();
-
-                    while (ketIt.hasNext()){
-                        SelectionKey key = ketIt.next();
-                        ketIt.remove();
-                        //处理对应key事件
-                        handler(key);
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-        }
-
-        private void handler(SelectionKey key) {
-            if(key.isValid()) {
-                key.readyOps();
-                //System.out.println("==>" key.);
-            }
-        }
+    private void accep(SelectionKey key) {
+    	ServerSocketChannel channel = (ServerSocketChannel)key.channel();
+    	try {
+			SocketChannel sc = channel.accept();
+			sc.configureBlocking(false);
+			Selector selector = key.selector();
+			sc.register(selector, SelectionKey.OP_READ);
+			System.out.println("客户端请求连接事件");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private void write(SelectionKey key) {
+    	ServerSocketChannel channel = (ServerSocketChannel)key.channel();
+    	try {
+    		
+			SocketChannel sc = channel.accept();
+			
+			ByteBuffer buffer = ByteBuffer.allocate(1);
+			
+			sc.read(buffer);
+			
+			buffer.flip();
+			
+			//System.out.println(new String(buffer.get));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    private void read(SelectionKey key) {
+    	
     }
 }
